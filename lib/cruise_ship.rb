75b8fec51dda51cf3fb9fe1c7b139079f8d95dc9
Kayla Cross <kac288@georgetown.edu>
2018-03-03 18:15:55 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -9,6 +9,7 @@ suite_e: "Crumpet the Elf"
 def select_winner(passengers)
   passengers.each do |suite, name|
     return name if suite == "suite_a" && name[0] = "A"
+  end
 end
 
 select_winner(passengers)
